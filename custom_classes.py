92452f76751066df9ec8433a021e4f282ff03cc2
KusaTea <koro4ka2@yandex.ru>
2023-07-26 15:04:00 +0200

    Normal-Anomaly process was changed

--- custom_classes.py
+++ custom_classes.py
@@ -51,7 +51,7 @@ class NasaDataset(Dataset):
 
     def __getitem__(self, idx: int) -> dict:
         if torch.is_tensor(idx):
-            return idx.tolist()
+            idx = idx.tolist()
         
         sample = {'sensors': self.dataset[idx], 'rul': self.ruls[idx], 'machine_id': self.machine_ids[idx]}
 
@@ -59,7 +59,76 @@ class NasaDataset(Dataset):
             sample = self.transfrom(sample)
 
         return sample
+
+
+class AnomalyDataset(Dataset):
+
+    def __init__(self, dataset_dict: dict = None,
+                 transform = None, device: torch.device = None):
+        
+        self.dataset = dataset_dict['sensors']
+        self.ruls = dataset_dict['rul']
+        self.machine_ids = dataset_dict['machine_id']
+
+        self.transfrom = transform
+
+        if device:
+            self.to(device)
     
+    def to(self, device):
+        self.dataset = self.dataset.to(device)
+        self.ruls = self.ruls.to(device)
+        self.machine_ids = self.machine_ids.to(device)
+
+    def get_input_shape(self) -> int:
+        return self.dataset.shape[1]
+    
+    def get_whole_dataset(self) -> tuple:
+        return self.dataset, self.machine_ids, self.ruls
+
+    def __len__(self) -> int:
+        return self.dataset.shape[0]
+
+    def __getitem__(self, indeces: int or list or torch.Tensor) -> dict:
+        if torch.is_tensor(indeces):
+            indeces = indeces.tolist()
+        
+        sample = {'sensors': self.dataset[indeces], 'rul': self.ruls[indeces], 'machine_id': self.machine_ids[indeces]}
+
+        if self.transfrom:
+            sample = self.transfrom(sample)
+
+        return sample
+    
+    def get_indeces(self, machine_id: int):
+        return torch.where(self.machine_ids == machine_id)[0]
+    
+
+class AnomalyLoader:
+    def __init__(self, dataset: AnomalyDataset, batch_size: int = None):
+        self.dataset = dataset
+        self.batch_size = batch_size if batch_size else len(dataset)
+
+        self.idx = 0
+        self.machine_ids = torch.unique(dataset.machine_ids)
+        self.current_machine_id = self.machine_ids[self.idx]
+        self.current_start = -batch_size
+        self.current_indeces = dataset.get_indeces(self.current_machine_id)
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current_start += self.batch_size
+        if self.current_start >= len(self.current_indeces):
+            self.idx += 1
+            if self.idx < len(self.machine_ids):
+                self.current_machine_id = self.machine_ids[self.idx]
+                self.current_indeces = self.dataset.get_indeces(self.current_machine_id)
+                self.current_start = 0
+            else:
+                raise StopIteration
+        return self.dataset[self.current_indeces[self.current_start:self.current_start + self.batch_size]]
 
 
 class SimpleAE(nn.Module):
@@ -123,12 +192,12 @@ def split_anomaly_normal(dataset: NasaDataset):
             'rul': ruls[ids]
         }
 
-        return NasaDataset(dataset_dict=dataset_dict)
+        return dataset_dict
     
 
     sensors, machine_ids, ruls = dataset.get_whole_dataset()
     normal_ids = torch.where(ruls == 125)[0]
     anomaly_ids = torch.where(ruls < 125)[0]
-    normal_data = __splitter(sensors, machine_ids, ruls, normal_ids)
-    anomaly_data = __splitter(sensors, machine_ids, ruls, anomaly_ids)
+    normal_data = NasaDataset(dataset_dict=__splitter(sensors, machine_ids, ruls, normal_ids))
+    anomaly_data = AnomalyDataset(dataset_dict=__splitter(sensors, machine_ids, ruls, anomaly_ids))
     return normal_data, anomaly_data
